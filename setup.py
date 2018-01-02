e4c03cd849bbe351eb62fabc60f44c469a5dd579
Arthur Allshire <arthur@allshire.org>
2018-01-02 07:40:41 -0500

    Create package directory structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='cassandra',
+      version='0.1',
+      description='The Drop Bears\' FRC prediction engine',
+      url='https://github.com/thedropbears/cassandra',
+      author='The Drop Bears',
+      license='MIT',
+      packages=['cassandra'],
+      install_requires=[
+      ],
+      zip_safe=False)
