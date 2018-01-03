eeabd2b3bde0603dd94ee0dc9bc3f9a8dccf4024
Arthur Allshire <arthur@allshire.org>
2018-01-03 17:10:27 -0500

    DataStore: add proper getters

--- cassandra/data_store.py
+++ cassandra/data_store.py
@@ -1,5 +1,6 @@
 import pickle
 import glob
+import logging
 
 from collections import OrderedDict
 from typing import Dict, List
@@ -22,7 +23,7 @@ class DataStore(object):
             year_events: For a new data store being created, the keys of this
             dictionary correspond to the years we will be storing matches for,
             and the values the events within those years. List of events must
-            be ordered by start date.
+            be ordered by start date (ie in chronological order).
         """
 
         self.cache_directory = cache_directory.rstrip('/')
@@ -72,8 +73,39 @@ class DataStore(object):
             matches: The list of matches to add to the data store.
 
         """
+        if event_code not in self.data[year].keys():
+            logging.warning("Event %s (year %s) is not an event in the data"
+                            "store, but matches for it are being added."
+                            % (event_code, year))
         self.data[year][event_code] = matches
         self.write_cache(year, self.data[year])
+
+    def get_year_events(self, year: int) -> List[str]:
+        """ Get the list of event codes for a year from the data store.
+
+        Args:
+            year: The year to get the list of events for.
+
+        Returns:
+            The list of events from that year, in chronological order.
+        """
+        return self.data[year].keys()
+
+    def get_event_matches(self, event_year: int, event_code: str) -> List:
+        """ Get the list of matches associated with event_code.
+
+        Args:
+            year: The year that event_code is part of.
+            event_code: The event code of the event we want that matches from.
+
+        Returns:
+            List of matches associated with event_code. `[]` if event_code was
+            supplied to constructor, but no matches have been added.
+        """
+        event_match_data = self.data[event_year][event_code]
+        # Always return a list,  but return an empty one if no matches have
+        # been added to this event.
+        return [] if event_match_data is None else event_match_data
 
     def write_cache(self, year: int, value):
         """ Write value to the cache for year. """
