2e91e5b285288d081bad5d5fb7da0753fa299f5d
Arthur Allshire <arthur@allshire.org>
2018-01-03 10:46:07 -0500

    Rename CacheManager->DataStore

--- cassandra/data_store.py
+++ cassandra/data_store.py
@@ -5,41 +5,42 @@ from collections import OrderedDict
 from typing import Dict, List
 
 
-class CacheManager(object):
+class DataStore(object):
 
     CACHE_FILE_EXTENSION = '-event_matches.p'
 
-    def __init__(self, cache_directory: str='cache', new_cache: bool=False,
-                 year_events: Dict[int, list]={}):
-        """Initialise the CacheManager class.
+    def __init__(self, cache_directory: str='cache',
+                 new_data_store: bool=False, year_events: Dict[int, list]={}):
+        """Initialise the DataStore class.
 
         Args:
-            cache_directory: Path to directory the cache files will be saved
-            in.
-            new_cache: Set to True to create a new cache with the structure of
-            year_events instead of using the current one.
-            year_events: For a new cache being created, the keys of this
-            dictionary correspond to the years we will be caching matches for,
+            cache_directory: Path to directory the data store's cache files
+            will be saved in. Defaults to `./cache/`.
+            new_data_store: Set to True to create a new data store with the
+            structure of year_events instead of using the one currently in
+            cache_directory.
+            year_events: For a new data store being created, the keys of this
+            dictionary correspond to the years we will be storing matches for,
             and the values the events within those years. List of events must
             be ordered by start date.
         """
 
         self.cache_directory = cache_directory.rstrip('/')
 
-        if new_cache:
+        if new_data_store:
             year_odicts = [(year, OrderedDict(
                 [(event_code, None) for event_code in events]))
                 for year, events in year_events.items()]
-            self.cache = OrderedDict(sorted(year_odicts,
-                                     key=lambda x: x[0]))
+            self.data = OrderedDict(sorted(year_odicts,
+                                           key=lambda x: x[0]))
 
-            # write the cache to disk
-            for year, year_odict in self.cache.items():
+            # write the data to disk
+            for year, year_odict in self.data.items():
                 self.write_cache(year, year_odict)
         else:
-            self.cache = OrderedDict()
+            self.data = OrderedDict()
 
-            # Find the cache files. Must be of the form:
+            # Find the data files. Must be of the form:
             # <year>-event_matches.p
             cache_file_pattern = ''.join([self.cache_directory, '/',
                                           '????',  # match year
@@ -55,23 +56,24 @@ class CacheManager(object):
             for fname in cache_files:
                 year_odict = pickle.load(open(fname, 'rb'))
                 year = get_year(fname)
-                self.cache[year] = year_odict
+                self.data[year] = year_odict
 
     def add_event_matches(self, year: int, event_code: str, matches: List):
-        """ Add matches to our cache.
+        """ Add matches to our data store.
 
         The matches are added to both this program instance's copy of the
-        cache (in this_instance.cache), and written to the disk.
+        data store (in this_instance.data), and written to the disk's cached
+        copy.
 
         Args:
             year: The year the matches need to be added to.
             event_code: The event code corresponding to the event the matches
             belong to.
-            matches: The list of matches to add to the cache.
+            matches: The list of matches to add to the data store.
 
         """
-        self.cache[year][event_code] = matches
-        self.write_cache(year, self.cache[year])
+        self.data[year][event_code] = matches
+        self.write_cache(year, self.data[year])
 
     def write_cache(self, year: int, value):
         """ Write value to the cache for year. """
