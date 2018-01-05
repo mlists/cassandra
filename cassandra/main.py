578a586560569ff8f7d933e2b361c2b01e89f644
joel <joel@joels-MacBook-Pro.local>
2018-01-05 21:17:16 +1100

    fetches events and matches with tba and caches

--- cassandra/main.py
+++ cassandra/main.py
@@ -1,12 +1,7 @@
 """Cassandra a prediction system for FRC games based on trueskill.
-
-Steps:
-    1 Load games from cache.
-    2 Get list of events from TBA.
-    3 Iterate through events, download and cache matches if not in cache.
 """
 
-import data_store
+from data_store import DataStore
 import requests
 
 
@@ -24,11 +19,31 @@ class Cassandra:
 
         # cache previous results
         events = {}
+        matches = {}
         base_url = "https://www.thebluealliance.com/api/v3"
         header = {"X-TBA-Auth-Key": self.key}
-        for year in years:
-            r = requests.get(base_url + "/events/" + str(year) + "/simple", headers=header)
-            r = r.json()
-            events[str(year)] = r
 
-        print(events["2010"])
+        # fetch events by year and order chronologically
+        for year in years:
+            r = requests.get(base_url + "/events/" + str(year) + "/simple", headers=header).json()
+
+            # sort by date and don't include offseason events
+            a = sorted(r, key=lambda b: b["start_date"])
+            a = [i["key"] for i in a if i["event_type"] < 99]
+
+            events[str(year)] = a
+
+        # fetch matches by year and event
+        for year in years:
+            for event in events[str(year)]:
+                r = requests.get(base_url + "/event/" + event + "/matches/simple", headers=header).json()
+
+                matches[event] = r
+
+        # save to cache
+        store = DataStore(new_data_store=True, year_events=events)
+
+        for year in years:
+            for event in events[str(year)]:
+                event_matches = matches[event]
+                store.add_event_matches(str(year), event, event_matches)
