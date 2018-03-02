3e5ea00e7b9a4dc5cdb8e5f17906c3a559a9de8e
Arthur Allshire <arthur@allshire.org>
2018-03-02 12:02:25 +1100

    Move TBA calls to datastore

--- cassandra/data_store.py
+++ cassandra/data_store.py
@@ -4,7 +4,7 @@ import logging
 import tbapy
 
 from collections import OrderedDict
-from typing import Dict, List
+from typing import List
 
 
 class DataStore(object):
@@ -12,7 +12,7 @@ class DataStore(object):
     CACHE_FILE_EXTENSION = '-event_matches.p'
 
     def __init__(self, cache_directory: str='cache',
-                 new_data_store: bool=False, year_events: Dict[int, list]={}):
+                 new_data_store: bool=False, years: list=[]):
         """Initialise the DataStore class.
 
         Args:
@@ -26,19 +26,36 @@ class DataStore(object):
             and the values the events within those years. List of events must
             be ordered by start date (ie in chronological order).
         """
+
         # First we need to monkey patch the tbapy.TBA class so we can
         # use the last-modified header
         def _new_tba_get(self, url):
             from requests import get
-            resp = get(self.URL_PRE + url, headers={'X-TBA-Auth-Key': self.auth_key, 'If-Modified-Since': self.last_modified})
+            resp = get(self.URL_PRE + url, headers={'X-TBA-Auth-Key': self.auth_key,
+                                                    'If-Modified-Since': self.last_modified})
+            self.last_modified_response = resp.headers['If-Modified-Since']
             return resp.json() if resp.status_code == 200 else {}
         tbapy.TBA._get = _new_tba_get
 
         # TODO - read the tba_auth_key from the file, or get it as an argument?
         self.tba = tbapy.TBA(self.tba_auth_key)
-        self.tba.last_modified = '' # e.g. 'Thu, 01 Mar 2018 17:21:48 GMT'
+        self.tba.last_modified = ''  # e.g. 'Thu, 01 Mar 2018 17:21:48 GMT'
 
         self.cache_directory = cache_directory.rstrip('/')
+
+        if not years:
+            years = range(2008, 2019)
+
+        year_events = {}
+        # fetch events by year and order chronologically
+        for year in years:
+            r = self.tba.events(year, simple=True)
+
+            # sort by date and don't include offseason events
+            a = sorted(r, key=lambda b: b["start_date"])
+            a = [i["key"] for i in a if i["event_type"] < 99]
+
+            year_events[year] = a
 
         if new_data_store:
             year_odicts = [(year, OrderedDict(
@@ -71,7 +88,14 @@ class DataStore(object):
                 year = get_year(fname)
                 self.data[year] = year_odict
 
-    def add_event_matches(self, year: int, event_code: str, matches: List):
+        # fetch matches by year and event
+        for year in years:
+            for event in year_events[year]:
+                r = self.fetch_event_matches(event)
+
+                self.data_store[year][event] = r
+
+    def add_event_matches(self, year: int, event_code: str, matches: List, last_modified: str):
         """ Add matches to our data store.
 
         The matches are added to both this program instance's copy of the
@@ -81,15 +105,17 @@ class DataStore(object):
         Args:
             year: The year the matches need to be added to.
             event_code: The event code corresponding to the event the matches
-            belong to.
+                belong to.
             matches: The list of matches to add to the data store.
+            last_modified: the last modified time of the match list being added
+                to the data store.
 
         """
         if event_code not in self.data[year].keys():
             logging.warning("Event %s (year %s) is not an event in the data"
                             "store, but matches for it are being added."
                             % (event_code, year))
-        self.data[year][event_code] = matches
+        self.data[year][event_code] = [last_modified, matches]
         self.write_cache(year, self.data[year])
 
     def get_year_events(self, year: int) -> List[str]:
@@ -114,10 +140,18 @@ class DataStore(object):
             List of matches associated with event_code. `[]` if event_code was
             supplied to constructor, but no matches have been added.
         """
-        event_match_data = self.data[event_year][event_code]
+        event_match_data = self.data[event_year][event_code][1]
         # Always return a list,  but return an empty one if no matches have
         # been added to this event.
         return [] if event_match_data is None else event_match_data
+
+    def fetch_event_matches(self, event_year: int, event_code: str) -> List:
+        """ Fetch event matches from TBA. """
+
+        self.last_modified = self.data[event_year][event_code][0]
+        self.data[event_year][event_code][0] = self.last_modified_response
+        matches = self.tba.event_matches[event_code]
+        return matches if matches else None
 
     def write_cache(self, year: int, value):
         """ Write value to the cache for year. """
--- cassandra/main.py
+++ cassandra/main.py
@@ -2,7 +2,6 @@
 """
 
 from data_store import DataStore
-import tbapy
 
 
 class Cassandra:
@@ -16,36 +15,4 @@ class Cassandra:
 
         self.years = years
         self.key = key
-
-        # cache previous results
-        events = {}
-        matches = {}
-        tba = tbapy.TBA(self.key)
-
-
-        # fetch events by year and order chronologically
-        for year in years:
-            r = tba.events(year, simple=True)
-
-            # sort by date and don't include offseason events
-            a = sorted(r, key=lambda b: b["start_date"])
-            a = [i["key"] for i in a if i["event_type"] < 99]
-
-            events[str(year)] = a
-
-        # fetch matches by year and event
-        for year in years:
-            for event in events[str(year)]:
-                r = tba.event_matches(event)
-
-                matches[event] = r
-
-        # save to cache
-        store = DataStore(new_data_store=False, year_events=events)
-
-        for year in years:
-            for event in events[str(year)]:
-                event_matches = matches[event]
-                store.add_event_matches(str(year), event, event_matches)
-
-        self.matches = matches
+        self.data_store = DataStore(new_data_store=False, years=self.years)
