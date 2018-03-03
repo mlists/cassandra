a8ea7182d360a852ad76975098fd89fa2763e6e1
michael <michael.kennedy@thedropbears.org.au>
2018-03-03 17:06:07 +1100

    Updated test to allow multiple years at the same time

--- Test.py
+++ Test.py
@@ -4,21 +4,22 @@ from cassandra.predictors import TrueSkillPredictor
 import sys
 
 if len(sys.argv) == 1:
-    sys.exit('ERROR: Not enough command arguments supplied. Need a ssh key.')
-year = 2016
-ds = DataStore(tba_auth_key=sys.argv[1], years=[year])
-empty_datastore = DataStore(years=[year], empty=True)
+    sys.exit('ERROR: Not enough command arguments supplied. Need an API key.')
+years = [2016, 2017]
+ds = DataStore(tba_auth_key=sys.argv[1], years=years)
+empty_datastore = DataStore(years=years, empty=True)
 tsp = TrueSkillPredictor(empty_datastore)
 brier = 0
 count = 0
-for event_code in ds.data[year]:
-    matches = ds.data[year][event_code]['matches']
-    for match, match_data in matches.items():
-        empty_datastore.add_single_match(year=year, event_code=event_code, match=match_data)
-        p_blue, p_red = tsp.predict(match_data['alliances']['blue']['team_keys'], match_data['alliances']['red']['team_keys'])
-        tsp.update(match)
-        outcome = 1 if match_data['winning_alliance'] == 'blue' else 0
-        brier += (p_blue - outcome) ** 2
-        count += 1
+for year in years:
+    for event_code in ds.data[year]:
+        matches = ds.data[year][event_code]['matches']
+        for match, match_data in matches.items():
+            empty_datastore.add_single_match(year=year, event_code=event_code, match=match_data)
+            p_blue, p_red = tsp.predict(match_data['alliances']['blue']['team_keys'], match_data['alliances']['red']['team_keys'])
+            tsp.update(match)
+            outcome = 1 if match_data['winning_alliance'] == 'blue' else 0
+            brier += (p_blue - outcome) ** 2
+            count += 1
 brier /= count
 print(brier)
